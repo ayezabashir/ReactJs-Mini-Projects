222c0f2dc5eef9af4e56ff40edabb09977f30d36
Ayeza <ayezabashir442@gmail.com>
2024-06-15 21:56:06 +0500

    accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
+import data from "./data";
+import './accordion.css'
+
 const Accordion = () => {
+    const [accordion, setAccordion] = useState(null);
+    const handleSelection = (id) => {
+        // console.log(id)
+        setAccordion(id)
+        // Close if the accordian is opened and clicked twice
+        if (id === accordion) {
+            setAccordion(null)
+        }
+    }
     return (
-        <>
-            accordion
-        </>
+        <div className="accordion">
+            {
+                data.map((item) => (
+                    <div className="item" key={item.id}>
+                        <h2 onClick={() => handleSelection(item.id)}>{item.question}</h2>
+                        {
+                            accordion === item.id ? <p>{item.answer}</p> : null
+                        }
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
