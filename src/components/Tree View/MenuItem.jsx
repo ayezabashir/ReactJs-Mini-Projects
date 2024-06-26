13c2c39a1595a8812b9b44b4c4bc22aca8fdf9d9
Ayeza <ayezabashir442@gmail.com>
2024-06-26 20:07:31 +0500

    styles

--- src/components/Tree View/MenuItem.jsx
+++ src/components/Tree View/MenuItem.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
 import MenuList from "./MenuList"
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 const MenuItem = ({ item }) => {
     const [displayCurrentChild, setDisplayCurrentChild] = useState({});
@@ -13,12 +14,12 @@ const MenuItem = ({ item }) => {
     }
     return (
         <li>
-            <div style={{ display: 'flex', gap: '20px' }}>
+            <div className="menu-item">
                 <p >{item.label}</p>
                 {
                     item && item.children && item.children.length > 0 ? <span style={{ cursor: "pointer" }} onClick={() => handleToggleChildren(item.label)}>
                         {
-                            displayCurrentChild[item.label] ? '-' : '+'
+                            displayCurrentChild[item.label] ? <FaMinus color="#fff" size={25} /> : <FaPlus color="#fff" size={25} />
                         }
                     </span> : null
                 }
