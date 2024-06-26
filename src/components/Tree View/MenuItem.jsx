6e75802f2dc8bcc68d2bfe57b9935ce0886596ab
Ayeza <ayezabashir442@gmail.com>
2024-06-26 17:08:44 +0500

    children items

--- src/components/Tree View/MenuItem.jsx
+++ src/components/Tree View/MenuItem.jsx
@@ -1,12 +1,30 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import MenuList from "./MenuList"
 
 const MenuItem = ({ item }) => {
+    const [displayCurrentChild, setDisplayCurrentChild] = useState({});
+
+    function handleToggleChildren(getCurrentLabel) {
+        setDisplayCurrentChild({
+            ...displayCurrentChild,
+            [getCurrentLabel]: !displayCurrentChild[getCurrentLabel]
+        })
+    }
     return (
         <li>
-            <p>{item.label}</p>
+            <div style={{ display: 'flex', gap: '20px' }}>
+                <p >{item.label}</p>
+                {
+                    item && item.children && item.children.length > 0 ? <span style={{ cursor: "pointer" }} onClick={() => handleToggleChildren(item.label)}>
+                        {
+                            displayCurrentChild[item.label] ? '-' : '+'
+                        }
+                    </span> : null
+                }
+            </div>
             {
-                item && item.children && item.children.length > 0 ?
+                item && item.children && item.children.length > 0 && displayCurrentChild[item.label] ?
                     <MenuList list={item.children} />
                     :
                     null
